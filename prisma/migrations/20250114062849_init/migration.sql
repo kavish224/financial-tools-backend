cc893bf78ff9c195a98bf57e80b3fd6c03adc015
Kavish Ambani <kavishkkambani@gmail.com>
2025-01-14 13:54:58 +0530

    update user model

---
+++ prisma/migrations/20250114062849_init/migration.sql
@@ -0,0 +1,43 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `created_at` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `firebase_uid` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `firstName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `profile_picture_url` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `User` table. All the data in the column will be lost.
+  - The `id` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - A unique constraint covering the columns `[uid]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `uid` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- DropIndex
+DROP INDEX "User_firebase_uid_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "created_at",
+DROP COLUMN "firebase_uid",
+DROP COLUMN "firstName",
+DROP COLUMN "lastName",
+DROP COLUMN "profile_picture_url",
+DROP COLUMN "updated_at",
+ADD COLUMN     "address" TEXT,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "displayName" TEXT,
+ADD COLUMN     "phoneNumber" TEXT,
+ADD COLUMN     "photoURL" TEXT,
+ADD COLUMN     "uid" TEXT NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_uid_key" ON "User"("uid");
