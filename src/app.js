e108f833a37e477849d0f4ee32ec4d070c684304
Kavish Ambani <kavishkkambani@gmail.com>
2025-01-20 11:06:14 +0530

    add profile routes

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-// Initialize the Express app
-const app = express();
+import profileRoutes from './routes/profile.routes.js';
 
-// Middleware
+dotenv.config();
+const app = express();
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true
-})); // Enable Cross-Origin Resource Sharing
-app.use(express.json({limit:"16kb"}))
+}));
+app.use(express.json({ limit: "16kb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "16kb" })); // Parse URL-encoded data
-app.use(express.static("public"))
-app.use(cookieParser())
-app.use(morgan("dev")); // Logging middleware for HTTP requests
-app.use(bodyParser.json()); // Parse incoming JSON requests
-
-// Base route
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Welcome to the Stock Recommendation API!");
 });
+app.use("/api/profile", profileRoutes);
 
-// Export the app instance
 export { app };
