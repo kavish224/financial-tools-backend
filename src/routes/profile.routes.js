3d6a8eafaad301ae123c49703a3cc6d5ce1fcba6
Kavish Ambani <kavishkkambani@gmail.com>
2025-06-03 08:24:25 +0530

    add role

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { syncUser } from '../controllers/user.controller.js';
 import { authenticateUser } from '../middlewares/authMiddleware.js';
+import { authorizeRole } from "../middlewares/authMiddleware.js";
+import { updateUserRole } from '../controllers/role.controller.js';
 
 const router = express.Router();
 
 router.post('/sync', authenticateUser, syncUser);
+router.post("/update-role", authenticateUser, authorizeRole(["ADMIN"]), updateUserRole);
 
 export default router;
