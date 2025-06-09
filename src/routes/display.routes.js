8696c6cd30f57eaacc36c2a40fa8215f86c0a733
Kavish Ambani <kavishkkambani@gmail.com>
2025-06-09 12:43:24 +0530

    add middleware to routes

--- src/routes/display.routes.js
+++ src/routes/display.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { n50, sma } from "../controllers/display.controller.js";
-import {  } from "../controllers/display.controller.js";
+import { authenticateUser } from "../middlewares/authMiddleware.js";
 const router = express.Router();
-router.get("/n-50",n50);
-router.post("/sma",sma);
+router.get("/n-50", authenticateUser, n50);
+router.post("/sma", authenticateUser,sma);
 export default router;
