f36aac240bd08fdcef3ac37dc4c7bda622537fb0
Kavish Ambani <kavishkkambani@gmail.com>
2025-06-16 01:45:02 +0530

    add requestLogger middleware for enhanced request logging and monitoring

---
+++ src/middlewares/requestLogger.js
@@ -0,0 +1,21 @@
+import { logger } from '../utils/logger.js';
+
+export const requestLogger = (req, res, next) => {
+  const start = Date.now();
+  
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    
+    logger.info('Request completed', {
+      method: req.method,
+      url: req.url,
+      statusCode: res.statusCode,
+      duration: `${duration}ms`,
+      ip: req.ip,
+      userAgent: req.get('User-Agent'),
+      user: req.user?.uid || 'Anonymous'
+    });
+  });
+  
+  next();
+};
